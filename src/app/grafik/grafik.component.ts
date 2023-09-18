1cad5b228cd35b6fee3d9e896f0a717380c93b5e
shyntaayu <shynta.add@gmail.com>
2023-09-18 21:19:53 +0700

    + filter daerah di line prediksi

--- src/app/grafik/grafik.component.ts
+++ src/app/grafik/grafik.component.ts
@@ -11,19 +11,28 @@ export class GrafikComponent implements OnInit, AfterViewInit {
     datasets: [],
   };
   title = "";
+  pilihan;
+  pilihanText = "";
+  tahun = "";
+  tahunAr = [];
+  dataLineAr = [];
+  daerah = [];
+  dataTemp = {};
 
   constructor() {
     let dataLine = localStorage.getItem("dataLine");
     this.title = localStorage.getItem("titleLine");
+    this.pilihan = localStorage.getItem("pilihan");
+    this.pilihanText = this.pilihan == 1 ? "Penyakit" : "Daerah";
     if (dataLine != undefined || dataLine != null || dataLine != "") {
-      let dataLineAr = [];
-      dataLineAr = JSON.parse(dataLine);
-      this.data.labels = Object.keys(dataLineAr[0]).filter(
+      this.dataLineAr = [];
+      this.dataLineAr = JSON.parse(dataLine);
+      this.data.labels = Object.keys(this.dataLineAr[0]).filter(
         (key) => key != "daerah_name"
       );
 
-      const restructuredData = dataLineAr.map((item) => {
-        const label = item.daerah_name;
+      const restructuredData = this.dataLineAr.map((item) => {
+        const label = this.pilihan == 1 ? item.daerah_name : item.penyakit_name;
         const values = Object.keys(item)
           .filter((key) => key !== "daerah_name")
           .map((key) => item[key]);
@@ -31,6 +40,7 @@ export class GrafikComponent implements OnInit, AfterViewInit {
       });
 
       this.data.datasets = restructuredData;
+      this.dataTemp = JSON.parse(JSON.stringify(this.data));
     }
     console.log("dataLineAr", this.data);
   }
@@ -39,5 +49,36 @@ export class GrafikComponent implements OnInit, AfterViewInit {
     // localStorage.removeItem("titleLine");
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tahunAr = this.getTahun(this.dataLineAr);
+    this.tahun = this.tahunAr.join(", ");
+  }
+
+  getTahun(dataArray) {
+    const years = dataArray.reduce((result, obj) => {
+      const objKeys = Object.keys(obj).filter((key) => /^\d{4}$/.test(key));
+      result.push(...objKeys);
+      return result;
+    }, []);
+
+    const uniqueYears = [...new Set(years)];
+
+    return uniqueYears;
+  }
+
+  filterAll() {
+    console.log("daerah", this.daerah);
+    let dataT = JSON.parse(JSON.stringify(this.data));
+    let filteredDatasets = dataT["datasets"].filter((dataset) =>
+      this.daerah.includes(dataset.label)
+    );
+
+    // Create a new object with the filtered datasets
+    let filteredData = {
+      labels: this.dataTemp["labels"],
+      datasets: filteredDatasets,
+    };
+    this.dataTemp = { ...filteredData };
+    console.log("aaa", filteredData);
+  }
 }
